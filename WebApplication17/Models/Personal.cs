8a0e8014280384ccc87246e7a7187ac0057eb054
JafarovMurad <cafarmuradmk@gmail.com>
2020-12-19 00:12:55 +0400

    New Project API

---
+++ WebApplication17/Models/Personal.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication17.Models
+{
+    public class Personal2
+    {
+        public int yash { get; set; }
+        public string name { get; set; }
+    }
+}
---
+++ WebApplication17/PersonalList.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication17.Controllers;
+using WebApplication17.Models;
+
+namespace WebApplication17
+{
+    public class PersonalList
+    {
+        public static List<Personal2> pers=new List<Personal2>
+        {
+            new Personal2
+            {
+                yash=27,
+                name="Murad"
+            },
+            new Personal2
+            {
+                yash=30,
+                name="Xaliq"
+                
+            },
+            new Personal2
+            {
+                yash=11,
+                name="Zaur"
+            }
+            
+        };
+    }
+}
